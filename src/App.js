0f25be59c2a45b3b8b482b577786cf60eff92436
shreyam462 <shreya.m462@gmail.com>
2024-12-09 15:20:40 +0530

    slight changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './navigation/MainNavigator';
 
 const App = () => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 export const API_BASE_URL = 'https://api.rapidmock.com/api/vikuman/v1';
 
-export const DEFAULT_MOVIE_IMAGE = require('../assets/images/placeholder.png');
-
 export const COLOR_PALETTE = {
     primary: '#007BFF',
     secondary: '#6C757D',
